74c3be64878169278e561bcb2576dc8eb3d5da28
nicolacucina <nicola.cucina@studenti.unipg.it>
2024-05-29 11:43:23 +0200

    added popularity to the legend

--- public/js/helper.js
+++ public/js/helper.js
@@ -5,6 +5,14 @@ const main_types = [
   { type: "Personification", color: "#1ac7c2", order: 4 },
 ];
 
+const popularity = [
+  { type : "0", radius : 7, order : 1},
+  { type : "1", radius : 9, order : 2},
+  { type : "2", radius : 11, order : 3},
+  { type : "3", radius : 13, order : 4},
+];
+
+
 const link_types = [
   { type: "Male Child", color: "#00b4d8", order: 1 },
   { type: "Female Child", color: "#ff70a6", order: 2 },
--- public/js/legend.js
+++ public/js/legend.js
@@ -17,6 +17,72 @@ const createLegend = () => {
     .attr("class", "legend-main-type-label")
     .text(d => d.type);
 
+  // Popularity
+  const popularityLegend = d3.select(".legend-popularity")
+    .append("svg")
+      .attr("viewBox", "0 0 340 110")
+    .append("g")
+      .attr("transform", "translate(1, 10)");
+
+  const popularityCircles = popularityLegend 
+    .append("g")
+      .attr("fill", "transparent")
+      .attr("stroke", "#272626");
+
+  const gap = 20;
+  popularityCircles
+    .append("circle")
+      .attr("cx", 13)
+      .attr("cy", 7)
+      .attr("r", 7);
+
+  popularityCircles
+    .append("circle")
+      .attr("cx", 13)
+      .attr("cy", 7+gap)
+      .attr("r", 9);
+
+  popularityCircles
+    .append("circle")
+      .attr("cx", 13)
+      .attr("cy", 11+gap*2)
+      .attr("r", 11);
+
+  popularityCircles
+  .append("circle")
+    .attr("cx", 13)
+    .attr("cy", 19+gap*3)
+    .attr("r", 13);
+  
+  const popularitylabels = popularityLegend
+    .append("g")
+      .attr("fill", "#272626")
+      .attr("dominant-baseline", "middle");
+
+  popularitylabels
+    .append("text")
+      .attr("x", gap*2)
+      .attr("y", 9)
+      .text("0 - 1.080.000");
+  
+  popularitylabels
+    .append("text")
+      .attr("x", gap*2)
+      .attr("y", 9+gap)
+      .text("1.080.000 - 2.740.000");
+  
+  popularitylabels
+    .append("text")
+      .attr("x", gap*2)
+      .attr("y", 13+gap*2)
+      .text("2.740.000 - 8.690.000");
+  
+  popularitylabels
+    .append("text")
+      .attr("x", gap*2)
+      .attr("y", gap*4)
+      .text("8.690.000 - 196.000.000");
+
   // Link Types
   const linkTypeLegend = d3.select(".legend-link-types")
     .append("ul")
